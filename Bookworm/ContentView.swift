7877e80b75dc690cee4892217f3fe37ee5df57a2
Sander Haug <sander_haug@hotmail.com>
2023-02-06 15:33:13 +0100

    Text Editor

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("Notes") private var notes = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            TextEditor(text: $notes)
+                .navigationTitle("Notes")
+                .padding()
         }
-        .padding()
     }
 }
 
